ed9df2beae144dbb3dceb8270a383fb724773501
iamganeshhegde <52537710+iamganeshhegde@users.noreply.github.com>
2019-08-13 19:29:27 +0530

    Update RestApiImpl.kt

--- app/src/main/java/com/example/weatherforecast/model/repository/networking/RestApiImpl.kt
+++ app/src/main/java/com/example/weatherforecast/model/repository/networking/RestApiImpl.kt
@@ -4,12 +4,9 @@ import com.example.weatherforecast.model.dataclass.WeatherForecast
 import io.reactivex.Observable
 
 class RestApiImpl : RestApi {
-
     var apiClient = ApiClient()
 
     override fun getWeatherForecast(key: String, searchQuery: String,days:Int): Observable<WeatherForecast> {
-
         return apiClient.connectToApi().getWeatherForecast(key, searchQuery,days)
-
     }
 }
